9aa08fc40f91c54be2a2209de40945137526906e
JuanJoseLL <1001367985@u.icesi.edu.co>
2024-05-18 18:15:42 -0500

    add login component and card

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,37 @@
 import Dashboard from "@/components/Dashboard";
+import { LoginButton } from "@/components/auth/login-button";
+import {Button} from "@/components/ui/button"
+import {cn} from "@/lib/utils"
+import { Poppins } from "next/font/google";
 
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["600"]
+})
+ 
 export default function Home() {
   return (
-    <main className="max-w-7xl mx-auto my-12">
-      <Dashboard/>
+    <main className="flex h-full flex-col
+     items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
+     <div className="spave-y-6">
+        <h1 className={cn("text-6xl font-semibold text-white drop-shadow-md",
+          font.className,
+        )}>
+          Auth
+        </h1>
+        <p className="text-white text-lg">
+          Get started by signing in with one of the following providers.
+        </p>
+        <div>
+          <LoginButton>
+            <Button variant="secondary" size="lg">
+              Sign in
+            </Button>
+          </LoginButton>
+        </div>
+     </div>
+
+
     </main>
   );
 }
