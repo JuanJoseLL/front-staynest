c17e757f0bede6225b9ca328f1890421446dfbde
JuanJoseLL <1001367985@u.icesi.edu.co>
2024-05-18 19:57:48 -0500

    add connection with the api

--- actions/register.ts
+++ actions/register.ts
@@ -1,5 +1,6 @@
 'use server';
 import * as z from 'zod';
+import axios from 'axios';
 
 import { RegisterSchema } from '@/schemas';
 
@@ -10,5 +11,24 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
         return {  error: 'Invalid email or password' }
     }
 
-    return { success: "Registration successful"}
+    //ESTE VALOR ES DE PRUEBA PARA PROBAR CON LA API
+    const valuesWithNewField = { ...validatedValues.data, role: 'USER' };
+
+    try{
+        const response = await axios.post('https://staynest.icybeach-62331649.eastus.azurecontainerapps.io/user/register', valuesWithNewField);
+        if( response.status === 200){
+            return { success: "Registration successful"}
+        } else if ( response.status === 400) {
+
+            return { error: "Email already is registered" }
+            
+        } else{
+            return { error: "Registration failed" }
+        }
+        
+    } catch (error){
+        console.log(error)
+        return { error: "An error occurred while trying to register" }
+    }
+   
 }
