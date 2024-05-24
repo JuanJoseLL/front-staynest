e1320e9c75cc79453ab092245fd11a88c43910f0
JuanJoseLL <1001367985@u.icesi.edu.co>
2024-05-24 09:46:10 -0500

    fix typo

--- auth.config.ts
+++ auth.config.ts
@@ -4,6 +4,7 @@ import type { NextAuthConfig } from "next-auth"
 import axios from 'axios';
 import GitHub from "next-auth/providers/github"
 import Google from "next-auth/providers/google"
+import { profile } from "console";
 
 
 
@@ -12,14 +13,8 @@ import Google from "next-auth/providers/google"
 
 export default { 
     providers: [
-        GitHub({
-            clientId: process.env.GITHUB_ID,
-            clientSecret: process.env.GITHUB_SECRET
-        }),
-        Google({
-            clientId: process.env.GOOGLE__CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET
-        }),
+        GitHub,
+        Google,
         Credentials({
             async authorize(credentials) {
                 const validatedValues = LoginSchema.safeParse(credentials);
