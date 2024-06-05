91865cbfe2083576e2a0f877ea8cd20cb643c6a1
JuanJoseLL <1001367985@u.icesi.edu.co>
2024-06-05 10:23:15 -0500

    reformat style

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -1,18 +1,27 @@
 import { Navbar } from "./_components/navbar";
+import React from 'react';
+
 interface ProtectedLayoutProps {
     children: React.ReactNode;
 }
 
-
 const ProtectedLayout = ({ children }: ProtectedLayoutProps) => {
-   
     return (
-        <div className="h-full w-full flex flex-col gap-y-10
-         items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
-            <Navbar />
-            {children}
+        <div className="relative h-full w-full">
+            <video
+                autoPlay
+                loop
+                muted
+                className="absolute inset-0 object-cover w-full h-full"
+            >
+                <source src="/video/5510387-hd_1920_1080_30fps.mp4" type="video/mp4" />
+            </video>
+            <div className="relative h-full w-full flex flex-col gap-y-10 items-center justify-center ">
+                <Navbar />
+                {children}
+            </div>
         </div>
-    )
-}
+    );
+};
 
 export default ProtectedLayout;
--- components/auth/header.tsx
+++ components/auth/header.tsx
@@ -18,7 +18,7 @@ export const Header = ({label}: HeaderProps) => {
                 "text-3xl font-semibold",
                 font.className,
             )}>
-                🔐 Auth
+                🔐 Log in
             </h1>
             <p className="text-muted-foreground text-sm">
                 {label}
