5b108899c8fc5699ddd9aafbaa65a2275ad8b05c
JuanJoseLL <1001367985@u.icesi.edu.co>
2024-06-05 10:28:59 -0500

    reformat navbar

--- app/(protected)/_components/navbar.tsx
+++ app/(protected)/_components/navbar.tsx
@@ -1,82 +1,53 @@
 'use client'
 
 import Link from "next/link";
-import { Button } from "@/components//ui/button"
+import { Button } from "@/components/ui/button"
 import { usePathname } from "next/navigation";
 import { UserButton } from "@/components/auth/user-button";
-import { useCurrentUser } from "@/hooks/use-current-user";
 import { useCurrentRole } from "@/hooks/use-current-role";
 import { NavRoleGate } from "./nav-role-gate";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
+import { useRouter } from 'next/navigation'
 
-import {useRouter} from 'next/navigation'
 export const Navbar = () => {
     const pathname = usePathname()
     const role = useCurrentRole()
     const router = useRouter()
 
-    useEffect (()=>{
-        if (role === undefined){
-            //window.location.reload()
+    useEffect(() => {
+        if (role === undefined) {
+            // window.location.reload()
         }
-        
+
         console.log(role)
 
-    },[])
-    
-    
+    }, [])
 
-   
-    
+
     return (
-        /**
-         * La navbar la puse así mientras tanto, pero se puede ajustar la altura y la anchura
-         * con top-? o w-? en el className del nav
-         */
-        <nav className="bg-secondary flex justify-between
-         items-center rounded-xl p-4 w-[1000px] shadow-sm mt-0 absolute top-10">
-            <div className="flex gap-x-2">
-                
-                {/* <Button
-                 asChild
-                 variant={pathname === "/admin" ? "default" : "outline"}
-                >
-                    <Link href="/admin">Admin</Link>
-
-                </Button> */}
-                {/* <Button
-                 asChild
-                 variant={pathname === "/settings" ? "default" : "outline"}
-                >
-                    <Link href="/settings">Settings</Link>
-
-                </Button> */}
-               
-                <Button
-                 asChild
-                 variant={pathname === "/properties" ? "default" : "outline"}
-                >
-                    <Link href="/properties">Properties</Link>
-
-                </Button>
-                <NavRoleGate allowedRole="ADMIN">
+        <nav className="bg-secondary flex justify-center items-center rounded-xl p-5 w-full shadow-sm fixed top-0 z-10">
+            <div className="flex justify-center items-center w-[1000px]">
+                <div className="flex gap-x-2">
+                    <Button
+                        asChild
+                        variant={pathname === "/properties" ? "default" : "outline"}
+                    >
+                        <Link href="/properties">Properties</Link>
+                    </Button>
+                    <NavRoleGate allowedRole="ADMIN">
                         <Button
                             asChild
                             variant={pathname === "/users" ? "default" : "outline"}
-                            >
+                        >
                             <Link href="/users">Users</Link>
-            
                         </Button>
-                </NavRoleGate>
-                    
-                
-               
+                    </NavRoleGate>
+                </div>
+                <p className="font-bold text-2xl mx-auto">STAYNEST</p>
+                <div className="flex items-center gap-x-4">
+                    <UserButton />
+                </div>
             </div>
-            <div className="flex items-center gap-x-2">
-                
-                <UserButton/>
-            </div>
-            
         </nav>
     )
 }
