778a20ac39f8d7b0d5491748f44fab7e7bfa222c
Dylan Bermudez Cardona <dyez1110@gmail.com>
2024-06-05 02:14:58 -0500

    feat: Add pagination and improve front

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,45 @@
 import { LoginButton } from "@/components/auth/login-button";
-import {Button} from "@/components/ui/button"
-import {cn} from "@/lib/utils"
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 import { Poppins } from "next/font/google";
 
 const font = Poppins({
   subsets: ["latin"],
-  weight: ["600"]
-})
- 
+  weight: ["600", "800"],
+});
+
 export default function Home() {
   return (
-    <main className="flex h-full flex-col
-     items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
-     <div className="space-y-6">
-        <h1 className={cn("text-6xl font-semibold text-center text-white drop-shadow-md",
-          font.className,
-        )}>
+    <main className="relative flex h-full flex-col items-center justify-center">
+      <video
+        autoPlay
+        loop
+        muted
+        className="absolute w-full h-full object-cover"
+      >
+        <source src="/video/5510387-hd_1920_1080_30fps.mp4" type="video/mp4" />
+      </video>
+      <div className="relative z-10 space-y-6 text-center">
+        <h1
+          className={cn(
+            "text-8xl font-extrabold text-white drop-shadow-lg",
+            font.className
+          )}
+        >
           Staynest
         </h1>
-        <p className="text-white text-lg">
+        <p className="text-white text-2xl font-light">
           Juan Jose Lopez, Dylan Bermudez, Sara Cardona
         </p>
-        <div className="text-center">
+        <div className="mt-6">
           <LoginButton mode="modal" asChild>
-            <Button variant="secondary" size="lg">
+            <Button variant="secondary" size="lg"  className="px-8 py-4 text-xl font-bold bg-white text-blue-800 rounded-full shadow-lg hover:bg-gray-100"
+          >
               Sign in
             </Button>
           </LoginButton>
         </div>
-     </div>
-
-
+      </div>
     </main>
   );
 }
